bedfd86279c13f4e12f28e9794a7f4773fcd0763
EC2 Default User <mikehallvideo@gmail.com>
2023-02-16 01:44:33 +0000

    google auth

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import AppLoginPage from "common/app/AppLoginPage"
+import google_clident_data from "admin/google_client_data.json"
+
+ReactDOM.render(
+   <GoogleOAuthProvider clientId={google_clident_data.web.client_id}>
+      <React.StrictMode>
+         <AppLoginPage app_name={"fracto-online"}/>
+      </React.StrictMode>
+   </GoogleOAuthProvider>,
+   document.getElementById('root')
+);
